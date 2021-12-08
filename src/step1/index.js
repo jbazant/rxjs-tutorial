db97d839ab742a407704049f9f0a521a394a3b20
Jiri Bazant <jiri.bazant@memsource.com>
2021-12-08 14:47:11 +0100

    rxjs lesson 1

--- src/step1/index.js
+++ src/step1/index.js
@@ -1,4 +1,33 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, combineLatest, startWith } from 'rxjs';
+import { map, withLatestFrom } from "rxjs/operators";
 
-const submitBtn = document.getElementById('submit-title')
-fromEvent(submitBtn, 'click').subscribe(() => console.log('Clicked!'));
+// https://dev.to/rxjs/fetching-data-in-react-with-rxjs-and-lt-gt-fragment-54h7
+// https://bazant.dev/tools/titles.php?prefix=asd
+
+
+const submitBtn = document.getElementById('submit-title');
+const list = document.getElementById('titles-list');
+
+const first$ = fromEvent(document.getElementById('article-first'), 'change')
+  .pipe(
+    map(it => it.target.value),
+    startWith('empty')
+  );
+first$.subscribe(it => console.log('first', it));
+
+const second$ = fromEvent(document.getElementById('article-second'), 'change')
+  .pipe(
+    map(it => it.target.value),
+    startWith('empty')
+);
+second$.subscribe(it => console.log('second', it));
+
+const full$ = combineLatest(first$, second$)
+  .pipe(map((vals) => vals.join(' ')));
+full$.subscribe((val) => console.log('full', val));
+
+fromEvent(submitBtn,'click')
+  .pipe(withLatestFrom(full$))
+  .subscribe(([_, name]) => {
+    list.innerHTML += `<li class="list-group-item">${name}</li>`
+  })
